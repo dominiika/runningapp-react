53458cdd368fbe63c23ab03ee9f6e108f5b58102
Dosia <dominika.gajda@gmail.com>
2020-11-21 18:58:20 +0100

    Delete a comment

--- src/components/TrainingsList.js
+++ src/components/TrainingsList.js
@@ -46,7 +46,6 @@ function TrainingsList(props) {
           {trainings && trainings.length > 0 ? (
             <React.Fragment>
               {trainings.map((training) => {
-                // return <p key={training.id}>{training.name}</p>;
                 return (
                   <div key={training.id}>
                     <ul className="collection z-depth-2">
@@ -65,83 +64,5 @@ function TrainingsList(props) {
     </React.Fragment>
   );
 }
-
-// class TrainingsList extends Component {
-//   state = {
-//     trainings: [],
-//     isLoading: true,
-//     prevToken: this.props.cookies.get('token'),
-//     fetched: false,
-//     // isActive: globalState.isActive
-//   };
-
-//   componentDidMount() {
-//     if (this.props.cookies.get('token')) {
-//       this.fetchTrainings();
-//       // } else {
-//       // this.setState({ fetched: false });
-//     }
-//   }
-
-//   componentDidUpdate() {
-//     let currentToken = this.props.cookies.get('token');
-//     if (this.state.prevToken !== currentToken && !this.state.fetched) {
-//       this.fetchTrainings();
-//       console.log('cur;=', currentToken);
-//       console.log('prev', this.state.prevToken);
-//     }
-//   }
-
-//   fetchTrainings = () => {
-//     fetch(`http://127.0.0.1:5000/trainings`, {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Authorization: `Bearer ${this.props.cookies.get('token')}`,
-//       },
-//     })
-//       .then((resp) => resp.json())
-//       .then((res) => {
-//         this.setState({
-//           trainings: res.trainings,
-//           isLoading: false,
-//           fetched: true,
-//         });
-//       })
-//       .catch((error) => console.log(error));
-//   };
-
-//   render() {
-//     console.log('prev in render', this.state.prevProps);
-//     console.log('context', Context);
-//     return (
-//       <React.Fragment>
-//         <Header title="Your trainings" />
-//         <Divider />
-//         <section>
-//           <div className="container">
-//             {this.state.trainings && this.state.trainings.length > 0 ? (
-//               <React.Fragment>
-//                 {this.state.trainings.map((training) => {
-//                   // return <p key={training.id}>{training.name}</p>;
-//                   return (
-//                     <div key={training.id}>
-//                       <ul className="collection z-depth-2">
-//                         <li className="collection-item avatar">
-//                           <Training training={training} />
-//                         </li>
-//                       </ul>
-//                     </div>
-//                   );
-//                 })}
-//               </React.Fragment>
-//             ) : null}
-//           </div>
-//         </section>
-//         <Divider />
-//       </React.Fragment>
-//     );
-//   }
-// }
 
 export default TrainingsList;
